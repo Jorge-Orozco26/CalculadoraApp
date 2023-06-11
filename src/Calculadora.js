2e2d2e537c884e90ff049d89e73bb863ca2c226c
Jorge Orozco <creneorozco@gmail.com>
2023-06-11 16:16:13 -0600

    Estilos agregados

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -24,4 +24,51 @@ const CalcularResultado =() => {
     } catch (error){
         setResultado('Error');
     }
-}
+};
+
+const limpiarResultado = () => {
+    setResultado('');
+};
+
+
+
+
+
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    resultContainer:{
+        marginBottom: 20,
+    },
+    resultText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    buttonContainer: {
+        width: 300,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 10,
+    },
+    button: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+        padding: 10,
+        borderRadius:  5,
+        margin: 4,
+    },
+    buttonText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+});
